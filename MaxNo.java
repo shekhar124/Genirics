1df54cf11f50b497233dd1f17477c3f85a389701
rajashekhar <rajashekharpasikanti@gmail.com>
2021-09-14 17:53:23 +0530

    Maximum of three Strings

--- MaxNo.java
+++ MaxNo.java
@@ -1,28 +1,30 @@
 package com.bl;
 
+import java.util.Scanner;
+
 public class MaxNo {
-	public static Float findMax(Float a, Float b, Float c) {
-		Float max = a;
+	public static String findMax(String a, String b, String c) {
+		String max = a;
 		if (b.compareTo(max) > 0)
 			max = b;
 		if (c.compareTo(max) > 0)
 			max = c;
-		System.out.printf("The max of three numbers %s %s %s is : %s \n", a, b, c, max);
+		System.out.printf("The max of three Strings \"%s\" \"%s\" \"%s\" is : %s \n", a, b, c, max);
 		return max;
 	}
 
 	public static void main(String[] args) {
-		// TestCase1 max no.is first position
-		Float f1 = 99.0f, f2 = 85.0f, f3 = 60.0f;
-		Float max = findMax(f1, f2, f3);
-
-		// TestCase2 max no.is second position
-		Float f4 = 34.0f, f5 = 85.0f, f6 = 25.0f;
-		Float max1 = findMax(f4, f5, f6);
-
-		// TestCase max no.is third position
-		Float f7 = 25.0f, f8 = 50.0f, f9 = 100.0f;
-		Float max2 = findMax(f7, f8, f9);
+		System.out.println("enter three strings");
+		Scanner scan = new Scanner(System.in);
+		// TestCase enter 3 strings
+		System.out.println("Enter the first String : ");
+		String str1 = scan.next();
+		System.out.println("Enter the second String : ");
+		String str2 = scan.next();
+		System.out.println("Enter the third String : ");
+		String str3 = scan.next();
+		String max = findMax(str1,str2,str3);
+		
 	}
 
 }
