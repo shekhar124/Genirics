91fbe651298231a3997870b38499b71b82ae787b
rajashekhar <rajashekharpasikanti@gmail.com>
2021-09-14 18:28:15 +0530

    using generic method

--- MaxNo.java
+++ MaxNo.java
@@ -1,30 +1,31 @@
 package com.bl;
 
-import java.util.Scanner;
-
 public class MaxNo {
-	public static String findMax(String a, String b, String c) {
-		String max = a;
-		if (b.compareTo(max) > 0)
-			max = b;
-		if (c.compareTo(max) > 0)
-			max = c;
-		System.out.printf("The max of three Strings \"%s\" \"%s\" \"%s\" is : %s \n", a, b, c, max);
+	// Refacter Generic method float,integer,String
+	public static <T extends Comparable<T>> T findMax(T[] Array) {
+
+		T max = Array[0];
+		for (T t : Array) {
+			if (t.compareTo(max) > 0)
+				max = t;
+		}
 		return max;
 	}
 
 	public static void main(String[] args) {
-		System.out.println("enter three strings");
-		Scanner scan = new Scanner(System.in);
-		// TestCase enter 3 strings
-		System.out.println("Enter the first String : ");
-		String str1 = scan.next();
-		System.out.println("Enter the second String : ");
-		String str2 = scan.next();
-		System.out.println("Enter the third String : ");
-		String str3 = scan.next();
-		String max = findMax(str1,str2,str3);
-		
-	}
 
+		Integer[] arrayOfInteger = { 343, 252, 453 };
+		Float[] arrayOfFloats = { 292.34f, 344.43f, 334.44f };
+		String[] arrayOfStrings = { "Are", "You", "Ok" };
+
+		Integer maxOfThreeIntegerValue = findMax(arrayOfInteger);
+		System.out.println("The maximum value of three integers is : " + maxOfThreeIntegerValue);
+
+		Float maxOfThreeFloats = findMax(arrayOfFloats);
+		System.out.println("The maximum value of three floats is : " + maxOfThreeFloats);
+
+		String maxOfThreeStrings = findMax(arrayOfStrings);
+		System.out.println("The maximum value of three strings is : " + maxOfThreeStrings);
+
+	}
 }
