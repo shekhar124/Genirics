a9692806be58b113da2470de935aa2cb3eb14652
rajashekhar <rajashekharpasikanti@gmail.com>
2021-09-14 17:27:04 +0530

    Find the maximum Floats

--- MaxNo.java
+++ MaxNo.java
@@ -1,30 +1,28 @@
 package com.bl;
 
 public class MaxNo {
-	public static Integer findMax(Integer a, Integer b, Integer c) {
-        Integer max = a;
-        if(b.compareTo(max)> 0)
-            max = b;
-        if(c.compareTo(max)>0)
-            max = c;
-        System.out.printf("The max of three numbers %s %s %s is : %s \n",a,b,c,max);
-        return max;
-    }
-    public static void main(String[] args) {
-        //TestCase1 max no.is first position
-        Integer int1 = 100,int2 = 50,int3 = 25;
-        Integer max = findMax(int1,int2,int3);
+	public static Float findMax(Float a, Float b, Float c) {
+		Float max = a;
+		if (b.compareTo(max) > 0)
+			max = b;
+		if (c.compareTo(max) > 0)
+			max = c;
+		System.out.printf("The max of three numbers %s %s %s is : %s \n", a, b, c, max);
+		return max;
+	}
 
-        //TestCase2 max no.is second position
-        Integer int4 = 50,int5 = 100,int6 = 25;
-        Integer max1 = findMax(int4,int5,int6);
+	public static void main(String[] args) {
+		// TestCase1 max no.is first position
+		Float f1 = 99.0f, f2 = 85.0f, f3 = 60.0f;
+		Float max = findMax(f1, f2, f3);
 
-        //TestCase max no.is third position
-        Integer int7 = 25,int8 = 50,int9 = 100;
-        Integer max2 = findMax(int7,int8,int9);
-    }
-	
+		// TestCase2 max no.is second position
+		Float f4 = 34.0f, f5 = 85.0f, f6 = 25.0f;
+		Float max1 = findMax(f4, f5, f6);
 
-	
+		// TestCase max no.is third position
+		Float f7 = 25.0f, f8 = 50.0f, f9 = 100.0f;
+		Float max2 = findMax(f7, f8, f9);
+	}
 
 }
